fea8907c6a7edd36419f2176272def633a47da3d
abd-allh <abdallah.ismail698@gmail.com>
2023-09-05 20:27:13 +0300

    Add Database

--- app.js
+++ app.js
@@ -1,66 +1,78 @@
+const express = require("express")
+const _ = require("lodash")
+const mongoose = require("mongoose")
+const { Post, defaultItems } = require("./Model")
 
-const express = require("express");
-const ejs = require("ejs");
-const _ = require("lodash");
-const port = process.env.PORT || 3000;
+require("dotenv").config({ path: "vars/.env" })
+const MUSER = process.env.USER
+const MPASS = process.env.PASS
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const app = express()
+const port = process.env.PORT || 3000
+const uri =
+  "mongodb+srv://" +
+  MUSER +
+  ":" +
+  MPASS +
+  "@abdallah.qb7z6xt.mongodb.net/blogDB?retryWrites=true&w=majority"
 
-const app = express();
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
-app.set('view engine', 'ejs');
+var posts = []
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+main().catch((err) => console.log(err))
+async function main() {
+  await mongoose.connect(uri)
+}
 
-var posts = [];
+app.get("/", async (req, res) => {
+  const foundPosts = await Post.find({})
 
-app.get("/", (req, res) => {
-  res.render("home", {startingContent: homeStartingContent, posts: posts});
-});
+  if (!(await Post.exists())) {
+    await Post.insertMany(defaultItems[1])
+    res.redirect("/")
+  } else {
+    res.render("home", {
+      startingContent: defaultItems[0],
+      posts: foundPosts,
+    })
+  }
+})
 
 app.get("/about", (req, res) => {
-  res.render("about", {aboutContent: aboutContent});
-});
+  res.render("about", { aboutContent: defaultItems[2] })
+})
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {contactContent: contactContent});
-});
+  res.render("contact", { contactContent: defaultItems[3] })
+})
 
 app.get("/compose", (req, res) => {
-  res.render("compose");
-});
+  res.render("compose")
+})
 
-app.get("/posts/:postTitle", (req, res) => {
+app.get("/posts/:postId", async (req, res) => {
+  const requestedPostId = req.params.postId
 
-  const requestedTitle = _.lowerCase(req.params.postTitle);
+  const post = await Post.findOne({ _id: requestedPostId })
 
-  posts.forEach(post => {
-    const storedTitle = _.lowerCase(post.title);
+  res.render("post", { title: post.title, content: post.content })
+})
 
-    if (storedTitle === requestedTitle) {
-      res.render("post", {title: post.title, content: post.content});
-    };
-
-  });
-
-});
-
-app.post("/compose", (req, res) => {
-
-  const post = {
+app.post("/compose", async (req, res) => {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-    url: "/posts/" + req.body.postTitle.replace(/\s+/g, '-').toLowerCase()
-  };
+    url: "/posts/" + req.body.postTitle.replace(/\s+/g, "-").toLowerCase(),
+  })
 
-  posts.push(post);
-  res.redirect("/");
-});
+  await post.save()
+  res.redirect("/")
+})
 
-app.listen(port, function(err){
-  if (err) console.log(err);
-  console.log(`Listening on PORT ${port}.`);
-});
+app.listen(port, function (err) {
+  if (err) console.log(err)
+  console.log(`Listening on PORT ${port}.`)
+})
